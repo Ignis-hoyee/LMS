ce595de22b931d33da82534924601e1e4b2c897b
IgnisHoyee <ignishoyee@outlook.com>
2018-06-18 18:53:53 +0800

    new update

--- Login.php
+++ Login.php
@@ -5,21 +5,22 @@
  * Date: 2018/3/28
  * Time: 16:33
  */
-$user = $_POST["user"];
-$password = $_POST["password"];
+function login(){
+
+    $user = $_POST["user"];
+    $password = $_POST["password"];
 
 
-$db_host = "localhost";
-$db_usr = "root";
-$db_pwd = "P@ssw0rd";
+    $db_host = "localhost";
+    $db_usr = "root";
+    $db_pwd = "P@ssw0rd";
 
-$conn = new mysqli($db_host, $db_usr, $db_pwd);
-if($conn -> connect_error){
-    die("Database Login failed." . $conn->connect_error);
+    $conn = new mysqli($db_host, $db_usr, $db_pwd);
+    if ($conn->connect_error) {
+        die("Database Login failed." . $conn->connect_error);
+    }
 }
 //数据库的登陆判断
-
-
 
 ?>
 
