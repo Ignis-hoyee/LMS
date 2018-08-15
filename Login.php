4f92692cb9389fdd117880722f6aff8039da96f4
ignis <ignishoyee@outlook.com>
2018-08-15 15:40:18 +0800

    登录功能已实现

--- Login.php
+++ Login.php
@@ -5,21 +5,39 @@
  * Date: 2018/3/28
  * Time: 16:33
  */
-$address = "localhost";
-$userid = "root";
-$passwd = "lhr19960813";
-$dbname = "user";
+$dbhost = "localhost";
+$dbuser= "root";
+$dbpswd = "lhr19960813";
+$dbname = "lms";
+$userid = $_POST['userid'];
+$passwd = $_POST['passwd'];
 
-$conn = mysqli_connect($address, $userid, $passwd, $dbname);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpswd, $dbname);
 
 if (!$conn){
     die("connection failed:" . mysqli_connect_error());
 }
-
-$sql = "SELECT number, user, password FROM user";
-$result = mysqli_query($conn, $sql);
-
-if(mysqli_num_rows($result) > 0){
-
+if ($userid && $passwd){
+    $sql = "SELECT * FROM user WHERE userid = '$userid' AND passwd = '$passwd';";
+    $result = mysqli_query($sql);
+    $rows = mysqli_num_rows($result);
+    if($row = 1){
+        header("refresh:0; url=management.html");
+        exit;
+    }else {
+        echo "用户或密码错误";
+        echo "
+            <script>
+                setTimeout(function(){window.location.href='login.html';},1000);
+            </script>
+            ";
+    }
+}else{
+    echo "用户名或密码不可为空";
+    echo "
+    <script>
+        setTimeout(function(){window.location.href='login.html';},1000);
+    </script>";
 }
+mysqli_close();
 ?>
